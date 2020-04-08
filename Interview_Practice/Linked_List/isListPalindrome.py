138238b2653443293ffafa3db311ddeffb54d29d
Vikrant Karawande <viku1996@gmail.com>
2020-04-08 19:11:24 +0530

    Create isListPalindrome.py

---
+++ Interview_Practice/Linked_List/isListPalindrome.py
@@ -0,0 +1,35 @@
+'''
+Note: Try to solve this task in O(n) time using O(1) additional space, where n is the number of elements in l, since this is what you'll be asked to do during an interview.
+
+Given a singly linked list of integers, determine whether or not it's a palindrome.
+
+Note: in examples below and tests preview linked lists are presented as arrays just for simplicity of visualization: in real data you will be given a head node l of the linked list
+
+Example
+
+For l = [0, 1, 0], the output should be
+isListPalindrome(l) = true;
+
+For l = [1, 2, 2, 3], the output should be
+isListPalindrome(l) = false.
+'''
+
+# Singly-linked lists are already defined with this interface:
+# class ListNode(object):
+#   def __init__(self, x):
+#     self.value = x
+#     self.next = None
+#
+def isListPalindrome(l):
+    if(l==None):
+        return True
+    elif(l.next==None):
+        return True
+    st = []
+    while(l!=None):
+        st.append(l.value)
+        l=l.next
+    for i in range(len(st)):
+        if(st[i]!=st[-1-i]):
+            return False
+    return True
