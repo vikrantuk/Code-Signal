acbd90209a539de714fa8c5410721ef9550a5361
Vikrant Karawande <viku1996@gmail.com>
2020-04-08 19:08:58 +0530

    Create removeKFromList.py

---
+++ Interview_Practice/Linked_List/removeKFromList.py
@@ -0,0 +1,33 @@
+'''
+Note: Try to solve this task in O(n) time using O(1) additional space, where n is the number of elements in the list, since this is what you'll be asked to do during an interview.
+
+Given a singly linked list of integers l and an integer k, remove all elements from list l that have a value equal to k.
+
+Example
+
+For l = [3, 1, 2, 3, 4, 5] and k = 3, the output should be
+removeKFromList(l, k) = [1, 2, 4, 5];
+For l = [1, 2, 3, 4, 5, 6, 7] and k = 10, the output should be
+removeKFromList(l, k) = [1, 2, 3, 4, 5, 6, 7].
+'''
+
+# Singly-linked lists are already defined with this interface:
+# class ListNode(object):
+#   def __init__(self, x):
+#     self.value = x
+#     self.next = None
+#
+def removeKFromList(l, k):
+    if(l==None):
+        return l
+    elif(l.value==k):
+        l=l.next
+    n=l
+    while(n!=None and n.next != None):
+        if(n.next.value==k):
+            n.next=n.next.next
+        else:
+            n=n.next
+    if(n!=None and n.value==k):
+        l=n.next
+    return l
