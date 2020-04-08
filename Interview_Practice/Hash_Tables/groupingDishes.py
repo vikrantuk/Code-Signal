88221dacc0d4d1979b9169a91263ab78843857df
Vikrant Karawande <viku1996@gmail.com>
2020-04-08 19:33:44 +0530

    Create groupingDishes.py

---
+++ Interview_Practice/Hash_Tables/groupingDishes.py
@@ -0,0 +1,49 @@
+'''
+You are given a list dishes, where each element consists of a list of strings beginning with the name of the dish, followed by all the ingredients used in preparing it. You want to group the dishes by ingredients, so that for each ingredient you'll be able to find all the dishes that contain it (if there are at least 2 such dishes).
+
+Return an array where each element is a list beginning with the ingredient name, followed by the names of all the dishes that contain this ingredient. The dishes inside each list should be sorted lexicographically, and the result array should be sorted lexicographically by the names of the ingredients.
+
+Example
+
+For
+  dishes = [["Salad", "Tomato", "Cucumber", "Salad", "Sauce"],
+            ["Pizza", "Tomato", "Sausage", "Sauce", "Dough"],
+            ["Quesadilla", "Chicken", "Cheese", "Sauce"],
+            ["Sandwich", "Salad", "Bread", "Tomato", "Cheese"]]
+the output should be
+  groupingDishes(dishes) = [["Cheese", "Quesadilla", "Sandwich"],
+                            ["Salad", "Salad", "Sandwich"],
+                            ["Sauce", "Pizza", "Quesadilla", "Salad"],
+                            ["Tomato", "Pizza", "Salad", "Sandwich"]]
+For
+  dishes = [["Pasta", "Tomato Sauce", "Onions", "Garlic"],
+            ["Chicken Curry", "Chicken", "Curry Sauce"],
+            ["Fried Rice", "Rice", "Onions", "Nuts"],
+            ["Salad", "Spinach", "Nuts"],
+            ["Sandwich", "Cheese", "Bread"],
+            ["Quesadilla", "Chicken", "Cheese"]]
+the output should be
+  groupingDishes(dishes) = [["Cheese", "Quesadilla", "Sandwich"],
+                            ["Chicken", "Chicken Curry", "Quesadilla"],
+                            ["Nuts", "Fried Rice", "Salad"],
+                            ["Onions", "Fried Rice", "Pasta"]]
+'''
+
+def groupingDishes(dishes):
+    hash = dict()
+    for dish in dishes:
+        dish_name=dish[0]
+        for i,ingredient in enumerate(dish):
+            if(i):
+                if(ingredient not in hash):
+                    hash[ingredient]=set()
+                hash[ingredient].add(dish_name)
+    print(hash)
+    temp = []
+    print(sorted(hash.keys()))
+    for ingredient in sorted(hash.keys()):
+        if(len(hash[ingredient])>1):
+            sube=[ingredient]
+            sube.extend(sorted(hash[ingredient]))
+            temp.append(sube)
+    return(temp)
