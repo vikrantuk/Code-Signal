fc5673632e273adca64f8b677b739d5849abce1f
Vikrant Karawande <viku1996@gmail.com>
2020-04-12 19:39:03 +0530

    Create 35_firstDigit.py

---
+++ Arcade/Intro/35_firstDigit.py
@@ -0,0 +1,17 @@
+'''
+Find the leftmost digit that occurs in a given string.
+
+Example
+
+For inputString = "var_1__Int", the output should be
+firstDigit(inputString) = '1';
+For inputString = "q2q-q", the output should be
+firstDigit(inputString) = '2';
+For inputString = "0ss", the output should be
+firstDigit(inputString) = '0'.
+'''
+
+def firstDigit(inputString):
+    for el in inputString:
+        if(el.isnumeric()):
+            return(el)
